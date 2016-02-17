4321ed3a75f3049f81e1a6e72a414bf935e71161
Michael Wasilczyk <michael@wasilczyk.com>
2016-02-16 22:52:41 -0500

    Removed alternate ideas

--- RelativityHistoryXMLtoOverlay/parsexml.py
+++ RelativityHistoryXMLtoOverlay/parsexml.py
@@ -2,54 +2,20 @@
 """
 Created on Tue Feb 16 20:44:17 2016
 
-@author: mikew
+To do:
+1. Create separate DAT overlay files for each field (DOCID + field)
+2. Check for a document with multiple updates to the same field, only keep
+    latest value.
+
 """
 
 import pandas as pd
 import xml.etree.ElementTree as ET
 
-"""
-print('=====================XML=====================')
-tree = ET.parse('test.xml')
-root = tree.getroot()
-
-for child in root:
-    print(child.tag, child.attrib)
-    print(child[-1].text)
-"""
-
-
-"""
-print('=====================CSV-READLINES=====================')   
-
-x = open('testcsv.csv', 'r').readlines()
-y = x[0]
-z = x[1:len(x)]
-
-columns = {'DOCID': 1}
-
-for i in z:
-    docid = i.split(',')[0]
-    rest = i[len(docid)+2:-2]
-    delilist = []
-    parsed = ET.fromstring(rest)
-    for child in parsed:
-        delilist.append(child[-1].text)
-    print(list(columns.keys()))
-    print(docid, ',', ' '.join(delilist))
-"""
-    
-print('=====================CSV-DATAFRAME=====================')
-
-"""
-To do:
-1. Create separate DAT overlay files for each field (DOCID + field)
-2. Check for a document with multiple updates to the same field, only keep
-    latest value.
-"""
+inputfile = 'testcsv.csv'
 
 # Reads CSV into memory as a dataframe, beware quotechar/CSV defaults
-df = pd.read_csv('testcsv.csv', quotechar='^')
+df = pd.read_csv(inputfile, quotechar='^')
 # Loop to parse XML for each row and create columns with field names
 for i in range(len(df)):
     # For current row, parse the XML
